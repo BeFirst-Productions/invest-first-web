967822d6133303f21a23784b151d098e4c8059ae
tldasarath <tldasarath@gmail.com>
2026-01-20 10:46:48 +0530

    :feat License section added

--- src/app/page.js
+++ src/app/page.js
@@ -1,18 +1,20 @@
 
 import HomeHeroSection from "@/Components/HeroSection/HomeHeroSection";
 import FormationandMeeting from "@/Components/HomePage/FormationandMeeting";
+import LicenseCategorySection from "@/Components/HomePage/LicenseCategorySection";
 import ProjectileCarousel from "@/Components/HomePage/ProjectileCarousel";
 import { logos } from "@/data/CarouselData";
 
 
 export default function Home() {
   return (
-   <>
-<div>
-  <HomeHeroSection/>
-  <FormationandMeeting/>
-      <ProjectileCarousel logos={logos} speed={0.8} />
-</div>
-   </>
+    <>
+      <div>
+        <HomeHeroSection />
+        <FormationandMeeting />
+        <ProjectileCarousel logos={logos} speed={0.8} />
+        <LicenseCategorySection />
+      </div>
+    </>
   );
 }
