5be0aad1673a67100ece4adc83cc0691e4629bc2
arjunnextmedia <arjunnextmedia@gmail.com>
2026-05-13 17:38:03 +0530

    feat: added common banner

--- src/components/layout/SectionContainer/SectionContainer.jsx
+++ src/components/layout/SectionContainer/SectionContainer.jsx
@@ -15,7 +15,7 @@ export default function SectionContainer({
       {background}
 
       {/* Main Content (inside max-w constrained container) */}
-      <div className={`relative max-w-[1440px] mx-auto px-[24px] md:px-[120px] z-10 ${containerClassName}`}>
+      <div className={`relative max-w-[1990px] mx-auto px-[24px] md:px-[120px] z-10 ${containerClassName}`}>
         {children}
       </div>
     </Component>
