fe9c8c7ea9126dfedede852ce04b57191426da8a
arjunnextmedia <arjunnextmedia@gmail.com>
2026-06-02 17:27:01 +0530

    feat: added favions

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,6 +26,9 @@ export const metadata = {
   metadataBase: new URL(
     process.env.NEXT_PUBLIC_SITE_URL || "http://localhost:3000",
   ),
+  appleWebApp: {
+    title: "invest first",
+  },
 };
 
 export default function RootLayout({ children }) {
